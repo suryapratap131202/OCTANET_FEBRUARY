726e70f26b271d463a92adc9023a4b05a884740a
Suryapratap Singh <suryapratap131202@gmail.com>
2024-02-07 17:38:20 +0530

    Task 1

---
+++ Landing Page/script.js
@@ -0,0 +1,31 @@
+function imgSlider(anything){
+    document.querySelector('.gshock').src = anything;
+ }
+
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+
+}
+
+var countDownDate = new Date("Feb 10, 2024 23:59:59").getTime();
+ 
+var x = setInterval(function() {
+ 
+  var now = new Date().getTime();
+ 
+  var distance = countDownDate - now;
+ 
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+ 
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+ 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000); 
